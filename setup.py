f83ab609e2cce1e5161b5b6a4c4153e3ec96b5a8
davide.bolognini <davide.bolognini@cloudacademy.com>
2017-09-27 13:06:52 +0200

    Fix name

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = "ca.disposable_email_checker",
-    version = "",
+    name = "disposable-email-checker",
+    version = "0.1",
     packages = find_packages(),
     author = "Giacomo Marinangeli",
     author_email = "giacomo@cloudacademy.com",
