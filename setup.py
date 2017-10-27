988679053bd917e81dba75f4ec161927d78b24fd
davide.bolognini <davide.bolognini@cloudacademy.com>
2017-10-27 19:28:58 +0200

    Force name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = "disposable_email_checker",
+    name = 'disposable_email_checker',
     version = "2.2",
     packages = find_packages(),
     author = "Giacomo Marinangeli",
